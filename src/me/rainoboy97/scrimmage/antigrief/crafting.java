51bf25576169c0d350ac1837a4eeba6cc8fe4a1e
bookofnathan <bookofnathan@gmail.com>
2013-09-15 15:52:35 -0500

    derp

--- src/me/rainoboy97/scrimmage/antigrief/crafting.java
+++ src/me/rainoboy97/scrimmage/antigrief/crafting.java
@@ -2,11 +2,13 @@ package me.rainoboy97.scrimmage.antigrief;
 
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class crafting {
     @EventHandler
     public void event_craft(PlayerInteractEvent event) {
+    if(!(event.getAction() == Action.RIGHT_CLICK_BLOCK)) return;
     if(!(event.getClickedBlock().getType() == Material.WORKBENCH)) return;
     event.setCancelled(true);
     event.getPlayer().openWorkbench(null, true);
